63a7900144b3329e9b8e916e1e5ff3608532337c
Niraj <whitedevil2188@gmail.com>
2024-09-28 12:14:17 +0530

    added ChatFeed.jsx

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App=()=>{
 457fb8fc-0ffa-476e-bbdd-f42aa6ea778c"   //project ki id chatengine.io se
             userName="nitin3647"    //user name of the user
             userSecret="2188"  //user ka password hoga
-            renderChatFeed={(ChatAppProps)=> <ChatFeed  {... chatAppProps />}}
+            renderChatFeed={(ChatAppProps)=> <ChatFeed  {... ChatAppProps }/>}
         />
     );
 }
