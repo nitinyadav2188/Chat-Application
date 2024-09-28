2d6ec3ff4a32e63d29abe27846f815447c5819c3
Niraj <whitedevil2188@gmail.com>
2024-09-28 13:04:31 +0530

    read-receipts

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -9,8 +9,31 @@ const ChatFeed = (props) => {
 
     const renderMessages = () => {
         const keys = Object.keys(messages);
-        console.log(keys);
+        
+        return keys.map((key, index) =>{
+           const message = messages[key];
+           const lastMessageKey = index === 0? null : keys[index-1];
+           const isMyMessage = userName === message.sender.userName;
+
+           return(
+            <div key={`msg_${index}`} style={{ width:'100%'}}>
+                <div className="message-block">
+                    {
+                        isMyMessage
+                        ? <MyMessage />
+                        : <TheirMessage />
+                    }
+                </div>
+                <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                read-receipts
+                </div>
+
+            </div>
+           )
+        })
     }
+
+    renderMessages()
 
     return (
         <div>
