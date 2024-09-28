75ad782b2aa79ad170c3ea8dc73a014a2fcfeed8
Niraj <whitedevil2188@gmail.com>
2024-09-28 13:20:53 +0530

    updated

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -20,8 +20,8 @@ const ChatFeed = (props) => {
                 <div className="message-block">
                     {
                         isMyMessage
-                        ? <MyMessage />
-                        : <TheirMessage />
+                        ? <MyMessage message={message} />
+                        : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
                     }
                 </div>
                 <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
@@ -35,12 +35,23 @@ const ChatFeed = (props) => {
 
     renderMessages()
 
-    return (
-        <div>
-            ChatFeed
-        </div>
-    )
+    if (!chat) return 'Loading....';
 
+    return (
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{height: '100px'}} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat}/>
+            </div>
+        </div>
+    );
 }
 
 export default ChatFeed;
